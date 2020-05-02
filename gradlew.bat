120a91505698032a701a96fea59941fcb3717e89
jangakniat <jangakniat@gmail.com>
2020-05-02 20:44:27 +0700

    Initial commit

---
+++ gradlew.bat
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<resources>
+    <plurals name="fui_error_weak_password"> <item quantity="one">पासवर्ड हवा तितका सक्षम नाही. कमीत कमी %1$d वर्ण आणि अंक व अक्षराचे मिश्रण वापरा</item> <item quantity="other">पासवर्ड हवा तितका सक्षम नाही. कमीत कमी %1$d वर्ण आणि अंक अक्षरांचे मिश्रण वापरा</item> </plurals>
+    <string name="fui_accessibility_logo">अ‍ॅप लोगो</string>
+    <string name="fui_auto_verified">फोन नंबरची अपोआप पडताळणी केली आहे</string>
+    <string name="fui_button_text_save">सेव्ह करा</string>
+    <string name="fui_button_text_send">पाठवा</string>
+    <string name="fui_cancel">रद्द करा</string>
+    <string name="fui_confirm_recovery_body">तुमचा पासवर्ड परत मिळवण्यासाठी %1$sवर पाठवलेल्या सूचनांचे पालन करा.</string>
+    <string name="fui_continue">सुरू ठेवा</string>
+    <string name="fui_email_account_creation_error">ईमेल खात्याची नोंदणी अयशस्वी झाली</string>
+    <string name="fui_email_hint">ईमेल</string>
+    <string name="fui_email_link_confirm_email_header">ईमेलची खात्री करा</string>
+    <string name="fui_email_link_confirm_email_message">साइन इन करणे सुरू ठेवण्यासाठी ईमेलची खात्री करा</string>
+    <string name="fui_email_link_cross_device_linking_text">तुमचे ईमेल खाते %1$s सह कनेक्ट करणे हा तुमचा मूळ उद्देश होता पण तुम्ही साइन इन न केलेल्या वेगळ्या डिव्हाइसवर लिंक उघडली आहे. \n\nतुम्हाला तरीही तुमचे %1$s खाते कन
